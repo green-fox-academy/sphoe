5ab4478cfac892881134a1fd9846db96e3a9ced9
Sphoe <zsofia.penzes@gmail.com>
2020-07-22 15:43:55 +0200

    Write new line

---
+++ Week02/Day03/src/WriteSingleLine.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WriteSingleLine {
+  public static void main(String[] args) {
+    // Write a function that is able to manipulate a file
+    // By writing your name into it as a single line
+    // The file should be named "my-file.txt"
+    // In case the program is unable to write the file,
+    // It should print the following error message: "Unable to write file: my-file.txt"
+    manipulateFile("Zsófi");
+  }
+    public static void manipulateFile(String myName){
+      List<String> content = new ArrayList();
+      content.add(myName);
+      try {
+        Path filePath = Paths.get("my-fil7e.txt");
+        Files.write(filePath, content);
+      }
+      catch (IOException e) {
+        System.out.println(" Unable to write file" + myName);
+
+
+    }
+      }
+    }
